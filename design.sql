716e8f1bbe0c40d6efe48d5e49b5c76dca7d159d
Prakhar <prakhar.kumar91@gmail.com>
2018-01-23 22:08:46 +0530

    made references

--- design.sql
+++ design.sql
@@ -1,28 +1,37 @@
+DROP table IF EXISTS student,course,teacher,section,registers,teaches;
+
+-- primary key has not null and unique
 create table student(
-    student_id text primary key not null,
+    student_id text primary key,
     name text
 );
 create table course(
-    course_id text primary key not null,
+    course_id text primary key,
     name text
 );
 create table teacher(
-    teacher_id text primary key not null,
+    teacher_id text primary key,
     name text
 );
 create table section(
-    section_number text primary key not null,
+    section_number text CHECK (section_number in ('A','B','C','D')),
+    course_id text not null unique references course(course_id),
+    primary key(section_number,course_id)
+    -- foreign key (course_id) ,
+    -- CONSTRAINT ref_integ UNIQUE course_id
 );
 
 
 
 create table registers(
-    student_id text not null,
-    course_id text not null,
-    primary key(student_id,course_id)
+    student_id text references teacher(teacher_id),
+    course_id text references course(course_id)
+    -- foreign key (teacher_id) references teacher(teacher_id)
+    -- foreign key (course_id) references course(course_id)
 );
 create table teaches(
-    teacher_id text not null,
-    course_id text not null,
-    primary key(teacher_id,course_id)
+    teacher_id text not null references teacher(teacher_id),
+    course_id text references course(course_id)
+    -- foreign key (teacher_id) references teacher(teacher_id)
+    -- foreign key (course_id) references course(course_id)
 );
