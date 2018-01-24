540880c55e8dc9e66606db72f53a3a87a640ec28
ankesh007 <ankeshgupta007@gmail.com>
2018-01-24 21:32:33 +0530

    Added test queries highlighting violations

--- test.sql
+++ test.sql
@@ -1,1 +1,2 @@
 INSERT INTO registers(student_id,course_id) VALUES ('cs1150667', '123');
+INSERT INTO registers(student_id,course_id) VALUES ('cs1150667', '123');
--- test_queries.sql
+++ test_queries.sql
@@ -1,3 +1,9 @@
 INSERT INTO student(student_id,name) VALUES ('cs1150667', 'Prakhar Kumar');
 INSERT INTO student(student_id,name) VALUES ('me1150668', 'Pranjal Singh');
+INSERT INTO course(course_id,name) VALUES ('col380','parallel');
+INSERT INTO registers(course_id,student_id) VALUES ('col380','cs1150667');
+INSERT INTO registers(course_id,student_id) VALUES ('col331','cs1150667'); --Error : Course not found
+INSERT INTO registers(course_id,student_id) VALUES ('col331','os'); --Error: 
+INSERT INTO registers(course_id,student_id) VALUES ('col380',NULL); --Error: NULL not allowed
+
 SELECT * FROM student;
