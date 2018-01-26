43a05356f28a6d47b1a580d962576aac81ee805d
ankesh007 <ankeshgupta007@gmail.com>
2018-01-26 23:55:35 +0530

    Updated Problem Statement and some minor changes

--- insert_tuples/fill.sh
+++ insert_tuples/fill.sh
@@ -3,6 +3,7 @@
 student='student.sql'
 course='course.sql'
 registers='registers.sql'
+query_size='500'
 rm -rf $student
 rm -rf $course
 rm -rf $registers
@@ -10,15 +11,16 @@ touch $student
 touch $course
 touch $registers
 
-for (( p=1; p <= 500000; p++ ))
+
+for (( p=1; p <= $query_size; p++ ))
 do
     echo "insert into student(student_id,name) values('$p',' ');" >> $student;
 done;
-for (( p=1; p <= 500000; p++ ))
+for (( p=1; p <= $query_size; p++ ))
 do
     echo "insert into course(course_id,name) values('$p',' ');" >> $course;
 done;
-for (( p=1; p <= 500000; p++ ))
+for (( p=1; p <= $query_size; p++ ))
 do
     echo "insert into registers(student_id,course_id) values('$p','$p');" >> $registers;
 done;
