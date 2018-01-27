36eaddc37fa26fc0db343c7f1605f5f9012a7ab8
Prakhar <prakhar.kumar91@gmail.com>
2018-01-27 20:34:34 +0530

    changed file path

--- Part2/bulk_load/registers.sql
+++ Part2/bulk_load/registers.sql
@@ -1,3 +1,3 @@
 copy registers from
-'/home/prakhar10_10/6th semester/COL362/Assignments/COL362-Assignment-1/bulk_load/registers.csv'
+'/home/prakhar10_10/6th semester/COL362/Assignments/COL362-Assignment-1/Part2/bulk_load/registers.csv'
 csv header;
--- Part2/bulk_load/student.sql
+++ Part2/bulk_load/student.sql
@@ -1,3 +1,3 @@
 copy student from
-'/home/prakhar10_10/6th semester/COL362/Assignments/COL362-Assignment-1/bulk_load/student.csv'
+'/home/prakhar10_10/6th semester/COL362/Assignments/COL362-Assignment-1/Part2/bulk_load/student.csv'
 csv header;
