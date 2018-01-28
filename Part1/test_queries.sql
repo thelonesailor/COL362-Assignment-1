cab1aa87cb3d0b28cf67507970faf5f66e4f4453
Prakhar <prakhar.kumar91@gmail.com>
2018-01-28 22:32:49 +0530

    uncommented some lines

--- Part1/test_queries.sql
+++ Part1/test_queries.sql
@@ -13,7 +13,7 @@ INSERT INTO course(course_id,name) VALUES ('col331','os');
 
 INSERT INTO registers(course_id,student_id) VALUES ('col380','me1150668');
 INSERT INTO registers(course_id,student_id) VALUES ('col331','cs1150667'); --Error : Course not found
--- INSERT INTO registers(course_id,student_id) VALUES ('col331','os'); --Error:
+INSERT INTO registers(course_id,student_id) VALUES ('col331','os'); --Error:
 INSERT INTO registers(course_id,student_id) VALUES ('col380',NULL); --Error: NULL not allowed
 
 DELETE FROM student WHERE student_id='cs1150667';
@@ -22,12 +22,7 @@ SELECT * FROM registers;
 SELECT * FROM teaches;
 SELECT * FROM section;
 
-CREATE VIEW stu_ids AS SELECT student_id FROM student;
-SELECT * FROM stu_ids;
 
-
-
-test for registers relation
 
 INSERT INTO student(student_id,name) VALUES ('cs1150667', ' ');
 INSERT INTO student(student_id,name) VALUES ('cs1150668', ' ');
@@ -45,25 +40,15 @@ INSERT INTO registers(student_id,course_id) VALUES ('cs1150667', null);--should 
 INSERT INTO registers(student_id,course_id) VALUES (null, 'c1');--should fail
 
 
-truncate table course; -- Error: Table "section" references "course" so cannot truncate a table referenced in a foreign key constraint.
-truncate table course cascade;
-truncate table student cascade;
-
 --test for teaches relation
+INSERT INTO teacher(teacher_id,name) VALUES ('t1', ' ');
+INSERT INTO teacher(teacher_id,name) VALUES ('t2', ' ');
 
--- INSERT INTO teacher(teacher_id,name) VALUES ('t1', ' ');
--- INSERT INTO teacher(teacher_id,name) VALUES ('t2', ' ');
---
--- INSERT INTO course(course_id,name) VALUES ('c1', ' ');
--- INSERT INTO course(course_id,name) VALUES ('c2', ' ');
---
--- INSERT INTO teaches(teacher_id,course_id) VALUES ('t1', 'c3');
--- INSERT INTO teaches(teacher_id) VALUES ('t1');--should fail
---
+INSERT INTO teaches(teacher_id,course_id) VALUES ('t1', 'c3');--should fail
+INSERT INTO teaches(teacher_id) VALUES ('t1');--should fail
+
 
 --test for section relation
-INSERT INTO course(course_id,name) VALUES ('c1', ' ');
-INSERT INTO course(course_id,name) VALUES ('c2', ' ');
 
 INSERT INTO section(section_number,course_id) VALUES ('A', 'c1');
 INSERT INTO section(section_number,course_id) VALUES ('A', 'c2');--should fail
